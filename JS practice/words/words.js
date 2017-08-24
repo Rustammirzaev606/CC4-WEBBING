593a51dabe1c94e1cd106c9115ea9ab4f869e8d4
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-08-24 13:22:34 -0400

    saving before big change

---
+++ JS practice/words/words.js
@@ -0,0 +1,17 @@
+function changeWords() {
+  document.getElementById('highlight').style.background = "lightblue";
+  document.getElementById('highlight').style.color = "white";
+}
+function jonJonesIsaCheater() {
+  document.getElementById('jjcheater').style.background = "yellow";
+  document.getElementById('jjcheater').style.color = "blue";
+  document.getElementById("jjcheater").style.fontFamily = "Impact,Charcoal,sans-serif";
+
+}
+function Bra() {
+  // document.getElementById('bro').style.background = "yellow";
+  // document.getElementById('bro').style.color = "blue";
+  // document.getElementById("bro").style.fontFamily = "Impact,Charcoal,sans-serif";
+  document.getElementsByClassName('a')[0].style.color = "blue";
+
+}
