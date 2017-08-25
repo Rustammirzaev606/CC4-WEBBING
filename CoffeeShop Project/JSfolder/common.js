bf3acf82569aca2afa0d055849b7dfd759185b9e
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-08-25 11:45:25 -0400

    save!

---
+++ CoffeeShop Project/JSfolder/common.js
@@ -0,0 +1,68 @@
+function lickMe() {
+  if (document.getElementById('_1').checked) {
+    confirm('Your order is on the way! TOTAL: $5')
+  }
+  else if (document.getElementById('_2').checked) {
+    confirm('Your order is on the way! TOTAL: $7')
+  }
+  else if (document.getElementById('_3').checked) {
+    confirm('Your order is on the way! TOTAL: $9')
+  }
+  else {
+    alert('choose something')
+  }
+  document.getElementById('_1').checked = false;
+  document.getElementById('_2').checked = false;
+  document.getElementById('_3').checked = false;
+}
+function lickMe2() {
+  if (document.getElementById('_4').checked) {
+    confirm('Your order is on the way! TOTAL: $5')
+  }
+  else if (document.getElementById('_5').checked) {
+    confirm('Your order is on the way! TOTAL: $7')
+  }
+  else if (document.getElementById('_6').checked) {
+    confirm('Your order is on the way! TOTAL: $9')
+  }
+  else {
+    alert('choose something')
+  }
+  document.getElementById('_4').checked = false;
+  document.getElementById('_5').checked = false;
+  document.getElementById('_6').checked = false;
+}
+function lickMe3() {
+  if (document.getElementById('_7').checked) {
+    confirm('Your order is on the way! TOTAL: $5')
+  }
+  else if (document.getElementById('_8').checked) {
+    confirm('Your order is on the way! TOTAL: $7')
+  }
+  else if (document.getElementById('_9').checked) {
+    confirm('Your order is on the way! TOTAL: $9')
+  }
+  else {
+    alert('choose something')
+  }
+  document.getElementById('_7').checked = false;
+  document.getElementById('_8').checked = false;
+  document.getElementById('_9').checked = false;
+}
+function lickMe4() {
+  if (document.getElementById('_10').checked) {
+    confirm('Your order is on the way! TOTAL: $5')
+  }
+  else if (document.getElementById('_11').checked) {
+    confirm('Your order is on the way! TOTAL: $7')
+  }
+  else if (document.getElementById('_12').checked) {
+    confirm('Your order is on the way! TOTAL: $9')
+  }
+  else {
+    alert('choose something')
+  }
+  document.getElementById('_10').checked = false;
+  document.getElementById('_11').checked = false;
+  document.getElementById('_12').checked = false;
+}
