211e550b84151487317a2eb5af7005eff7eecfec
rustammirzaev606 <rustammirzaev606@gmail.com>
2017-08-29 13:09:09 -0400

    fixed it

--- homework/week10day1/hiding.js
+++ homework/week10day1/hiding.js
@@ -1,11 +1,15 @@
-// function meme() {
-//   $('h2').hide();
-// }
-// $("#strtMgc1").click(function() {
-//   $(h2).hide()
-// });
-$(document).ready(function(){
-    $("#strtMgc1").click(function(){
-        $("h").hide();
-    });
+
+$("#strtMgc1").click(function(){
+  $("h1").toggle();
+  $("h2").hide();
+  $("h3").hide();
+  $("p").hide();
+});
+
+$("#strtMgc2").click(function() {
+  $('h1').show();
+});
+
+$(".oneTwoTrip").click(function(){
+  $(".first").fadeIn(1500);
 });
